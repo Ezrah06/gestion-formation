de36b08878a7d796396f18a574e6f9b4529bdb1c
ezrah <mampiononaezrah@gmail.com>
2026-05-05 13:57:29 +0200

    Projet Laravel gestion formation

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -9,7 +9,7 @@ class FormationController extends Controller
      // Liste des formations
     public function index()
     {
-        return response()->json(Formation::all());
+        return response()->json(formation::all());
     }
 
     // Ajouter une formation
@@ -21,7 +21,7 @@ class FormationController extends Controller
             'prix' => 'required|numeric'
         ]);
 
-        $formation = Formation::create($request->all());
+        $formation = formation::create($request->all());
 
         return response()->json([
             'message' => 'Formation ajoutée avec succès',
@@ -32,13 +32,13 @@ class FormationController extends Controller
     // Afficher une formation
     public function show($id)
     {
-        return response()->json(Formation::findOrFail($id));
+        return response()->json(formation::findOrFail($id));
     }
 
     // Modifier une formation
     public function update(Request $request, $id)
     {
-        $formation = Formation::findOrFail($id);
+        $formation = formation::findOrFail($id);
 
         $formation->update($request->all());
 
@@ -51,7 +51,7 @@ class FormationController extends Controller
     // Supprimer une formation
     public function destroy($id)
     {
-        $formation = Formation::findOrFail($id);
+        $formation = formation::findOrFail($id);
         $formation->delete();
 
         return response()->json([
